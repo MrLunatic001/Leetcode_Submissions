785fe6e2ff453982c30e5a62a897c338fbba081a
Adriel Au <63085003+MrLunatic001@users.noreply.github.com>
2026-09-03 11:40:31 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (34.44%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,20 +1,17 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-        def backtrack(start: int, comb: List[int], current_sum: int):
-            # Base cases: stop early if combination exceeds length k or target sum n
+        def backtrack(start,comb,currentSum):
             if len(comb) == k:
-                if current_sum == n:
-                    res.append(list(comb))
+                if sum(comb) == n:
+                    res.append(comb)
                 return
-            
-            # Explore numbers from 'start' to 9 to ensure strictly increasing order
-            for i in range(start, 10):
-                if current_sum + i > n:
-                    break  # Prune search space if adding 'i' exceeds target sum
-                
-                backtrack(i + 1, comb + [i], current_sum + i)
-                
-        backtrack(1, [], 0)
+
+            for i in range(start,10):
+                if currentSum + i > n:
+                    break
+                backtrack(i+1,comb + [i], currentSum + i)
+
+        backtrack(1,[],0)
 
         return res
