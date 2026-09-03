ec505547e6d68325a66c32043c12e69cd1a1d368
Adriel Au <63085003+MrLunatic001@users.noreply.github.com>
2026-09-03 11:29:11 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (78.63%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -12,13 +12,12 @@ class Solution:
             '8': 'tuv',
             '9': 'wxyz',
         }
-        def backtrack(idx, comb):
+        def backtrack(idx,comb):
             if idx == len(digits):
-                res.append(comb[:])
+                res.append(comb)
                 return
-            
             for letter in digit_to_letters[digits[idx]]:
-                backtrack(idx + 1, comb + letter)
+                backtrack(idx+1,comb+letter)
 
         res = []
         backtrack(0, "")
