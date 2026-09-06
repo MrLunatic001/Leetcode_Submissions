6dfe56b19403544152b2543045594252e7cb154c
Adriel Au <63085003+MrLunatic001@users.noreply.github.com>
2026-09-06 14:33:24 +0100

    Time: 8 ms (95.85%) | Memory: 20.6 MB (8.17%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,17 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0
-        
-        for i, jump in enumerate(nums):
-            # If the current index is beyond the furthest reachable point, stop
-            if i > max_reach:
-                return False
-            
-            # Update the max index reachable from here
-            max_reach = max(max_reach, i + jump)
-            
-            # Early return if we can already reach or exceed the last index
-            if max_reach >= len(nums) - 1:
-                return True
-                
-        return True
+        goal = len(nums) - 1
+
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
